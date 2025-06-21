1b6876211d6d3239e1bc8fab41fcabc2839ee34a
fawwasaldy <fawwas.aldy2111@gmail.com>
2025-06-21 21:46:53 +0700

    refactor: rename parameters in donation repository methods for clarity

--- infrastructure/database/donation/donated_item_recipient/repository.go
+++ infrastructure/database/donation/donated_item_recipient/repository.go
@@ -17,7 +17,7 @@ func NewRepository(db *transaction.Repository) donated_item_recipient.Repository
 	}
 }
 
-func (r repository) Create(ctx context.Context, tx interface{}, donatedItemCategoryEntity donated_item_recipient.DonatedItemRecipient) (donated_item_recipient.DonatedItemRecipient, error) {
+func (r repository) Create(ctx context.Context, tx interface{}, donatedItemRecipientEntity donated_item_recipient.DonatedItemRecipient) (donated_item_recipient.DonatedItemRecipient, error) {
 	validatedTransaction, err := validation.ValidateTransaction(tx)
 	if err != nil {
 		return donated_item_recipient.DonatedItemRecipient{}, err
@@ -28,16 +28,16 @@ func (r repository) Create(ctx context.Context, tx interface{}, donatedItemCateg
 		db = r.db.DB()
 	}
 
-	donatedItemCategorySchema := EntityToSchema(donatedItemCategoryEntity)
-	if err = db.WithContext(ctx).Create(&donatedItemCategorySchema).Error; err != nil {
+	donatedItemRecipientSchema := EntityToSchema(donatedItemRecipientEntity)
+	if err = db.WithContext(ctx).Create(&donatedItemRecipientSchema).Error; err != nil {
 		return donated_item_recipient.DonatedItemRecipient{}, err
 	}
 
-	donatedItemCategoryEntity = SchemaToEntity(donatedItemCategorySchema)
-	return donatedItemCategoryEntity, nil
+	donatedItemRecipientEntity = SchemaToEntity(donatedItemRecipientSchema)
+	return donatedItemRecipientEntity, nil
 }
 
-func (r repository) Update(ctx context.Context, tx interface{}, donatedItemCategoryEntity donated_item_recipient.DonatedItemRecipient) (donated_item_recipient.DonatedItemRecipient, error) {
+func (r repository) Update(ctx context.Context, tx interface{}, donatedItemRecipientEntity donated_item_recipient.DonatedItemRecipient) (donated_item_recipient.DonatedItemRecipient, error) {
 	validatedTransaction, err := validation.ValidateTransaction(tx)
 	if err != nil {
 		return donated_item_recipient.DonatedItemRecipient{}, err
@@ -48,11 +48,14 @@ func (r repository) Update(ctx context.Context, tx interface{}, donatedItemCateg
 		db = r.db.DB()
 	}
 
-	donatedItemCategorySchema := EntityToSchema(donatedItemCategoryEntity)
-	if err = db.WithContext(ctx).Updates(&donatedItemCategorySchema).Error; err != nil {
+	donatedItemRecipientSchema := EntityToSchema(donatedItemRecipientEntity)
+	if err = db.WithContext(ctx).
+		Where("donated_item_id = ?", donatedItemRecipientSchema.DonatedItemID).
+		Where("recipient_id = ?", donatedItemRecipientSchema.RecipientID).
+		Updates(&donatedItemRecipientSchema).Error; err != nil {
 		return donated_item_recipient.DonatedItemRecipient{}, err
 	}
 
-	donatedItemCategoryEntity = SchemaToEntity(donatedItemCategorySchema)
-	return donatedItemCategoryEntity, nil
+	donatedItemRecipientEntity = SchemaToEntity(donatedItemRecipientSchema)
+	return donatedItemRecipientEntity, nil
 }
